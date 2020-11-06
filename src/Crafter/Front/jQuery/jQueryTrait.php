590fa0670fa7b0f851d9f43b4d46e15f9631f90e
Vlad <salabunv@ukr.net>
2020-11-06 12:13:56 +0200

    1

--- src/Crafter/Front/jQuery/jQueryTrait.php
+++ src/Crafter/Front/jQuery/jQueryTrait.php
@@ -14,9 +14,32 @@ trait jQueryTrait
 
     public function getAjaxCreateSourceCode() 
     {
-        // TODO:
-        var_dump($this->getEntities());
-        return 222;
+        $sourceCode = new CodeWriter;
+
+        // На початку кожного файлу додаю свої копірайти:
+        $sourceCode->line('$.ajax({')->defaultSpaces(4);   
+            $sourceCode->line('type: "POST",');   
+            $sourceCode->line('url: SERVER_URL + "/api/paragraphs",');   
+            $sourceCode->line('data: {'); 
+
+            
+            $sourceCode->line('}')->defaultSpaces(0);   
+            
+    $sourceCode->line('}).done (function (response) {')->defaultSpaces(4);
+            $sourceCode->line('console.log("ajax create:", response);')->defaultSpaces(0);
+        $sourceCode->line('});');
+/*
+    
+    
+        id: 111111,
+        text: '$flight->refresh();',
+        order_id: 'Кеды',
+        data: 'Кеды'
+
+})
+*/
+
+        return $sourceCode->getCode(); 
     }
     
     public function getAjaxReadSourceCode() 
